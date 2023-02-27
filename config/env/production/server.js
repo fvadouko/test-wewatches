913bfbb8342e9d4c08ea52ff1c829cebc89ea148
ADOUKO Franck Venance <fvadouko@live.fr>
2023-02-27 20:06:18 +0000

    Bug: fix by setting ssl to true

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,4 +1,10 @@
 module.exports = ({ env }) => ({
   url: env("WEBSITE_URL"),
   port: process.env.PORT,
+  app: {
+    keys: env.array("APP_KEYS", [
+      "xK3ahudoDuLGx8gRp3FSew==",
+      "3jyMlXONvwxn0z+oi/0N/A==",
+    ]),
+  },
 });
