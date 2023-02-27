85e5f4461c6a2c25d7cb59823d2f61ad33e8e10c
ADOUKO Franck Venance <fvadouko@live.fr>
2023-02-27 20:25:06 +0000

    Check value of config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,21 @@
 const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
+console.log(
+  "config: ",
+  config,
+  "process.env.DATABASE_URL: ",
+  process.env.DATABASE_URL,
+  "config.host",
+  config.host,
+  "config.port",
+  config.port,
+  "config.database",
+  config.database,
+  "config.user",
+  config.user,
+  "config.password",
+  config.password
+);
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
